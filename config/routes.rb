60482227f1009c3489811b03d251b52ff1243fce
Mark McDermott <mark@markmcdermott.io>
2022-05-19 22:07:23 -0500

    header/footer, static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get    '/login',    to: 'sessions#new'
+  post   '/login',    to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
+  get    '/signup',   to: 'users#new'
+  post   '/signup',   to: 'users#create'
+  get    '/about',    to: 'static_pages#about'
+  get    '/contact',  to: 'static_pages#contact'
 
 end
